b4fbd8eca37fa78821d43814e7049ebbd776ae5e
drood87 <dbernstein1987@hotmail.com>
2019-06-11 16:44:16 +0800

    doing stuff with react spring

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,13 +10,14 @@ import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 import styled from 'styled-components';
 import Img from 'gatsby-image';
+import { Spring } from 'react-spring/renderprops';
 import Header from './header';
 import Archive from './archive';
 import './layout.css';
 
 const MainLayout = styled.main`
   max-width: 90%;
-  margin: 0 auto 2rem auto;
+  margin: 1rem auto 2rem auto;
   display: grid;
   grid-template-columns: 3fr 1fr;
   grid-gap: 40px;
@@ -44,9 +45,19 @@ const Layout = ({ children, location }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        {location.pathname === '/' && (
-          <Img fluid={data.file.childImageSharp.fluid} />
-        )}
+        <Spring
+          from={{ height: location.pathname === '/' ? 100 : 200 }}
+          to={{ height: location.pathname === '/' ? 200 : 100 }}
+        >
+          {styles => (
+            <div style={{ overflow: 'hidden', ...styles }}>
+              <Img fluid={data.file.childImageSharp.fluid} />
+            </div>
+          )}
+        </Spring>
+        {/* {location.pathname === '/' && (
+
+        )} */}
 
         <MainLayout>
           <div>{children}</div>
