a615c3e48fc7a31239e0240c04af9e865522e571
drood87 <dbernstein1987@hotmail.com>
2019-06-11 14:40:12 +0800

    install styled components and pull blogs from mds

---
+++ src/components/listing.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link, StaticQuery, graphql } from 'gatsby';
+
+import Image from '../components/image';
+
+const LISTING_QUERY = graphql`
+  query BlogPostListing {
+    allMarkdownRemark(
+      limit: 10
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            date(formatString: "DD MMMM YYYY")
+            title
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Listing = () => (
+  <StaticQuery
+    query={LISTING_QUERY}
+    render={({ allMarkdownRemark }) =>
+      allMarkdownRemark.edges.map(({ node }) => (
+        <article key={node.frontmatter.slug}>
+          <Link to={`/posts${node.frontmatter.slug}`}>
+            <h2>{node.frontmatter.title}</h2>
+          </Link>
+          <p>{node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+          <Link to={`/posts${node.frontmatter.slug}`}>Read more</Link>
+        </article>
+      ))
+    }
+  />
+);
+
+export default Listing;
--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import Layout from '../components/layout';
+import { graphql, Link } from 'gatsby';
+
+//Static query can be used anywhere
+//Does not accept variables
+//can't use context
+
+//Page Query
+//Must be used on pages
 
 export default class postLayout extends Component {
   render() {
+    const { markdownRemark } = this.props.data;
     return (
       <Layout>
         <div>
-          <h1>Post Layout</h1>
+          <h1>{markdownRemark.frontmatter.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: markdownRemark.html,
+            }}
+          />
         </div>
+        <Link to={`/`}>Back to Front</Link>
       </Layout>
     );
   }
 }
+
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date
+        slug
+      }
+    }
+  }
+`;
